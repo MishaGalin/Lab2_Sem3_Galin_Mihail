946e06583f3a375910c56fff68fad833862643f3
Mikhail Galin <mishagalin85@gmail.com>
2021-11-21 22:17:33 +0500

    Раздел 2

--- GraphObject.cs
+++ GraphObject.cs
@@ -5,27 +5,46 @@ namespace Lab2_Sem3_Galin_Mihail
 {
     public class GraphObject
     {
+        private static Random r = new Random();
+        private SolidBrush brush;
         private Color c;
         private int x, y, w, h;
-
-        private SolidBrush brush;
-        private static Random r = new Random();
 
         public GraphObject()
         {
             Color[] cols = { Color.Red, Color.Green, Color.Yellow, Color.Tomato, Color.Cyan };
             c = cols[r.Next(cols.Length)];
-            x = r.Next(200);
-            y = r.Next(200);
+            X = r.Next(200);
+            Y = r.Next(200);
             w = 50;
             h = 50;
             brush = new SolidBrush(c);
         }
 
+        public int X
+        {
+            get => x;
+            set
+            {
+                if (value < 0) throw new ArgumentException("x<0!");
+                x = value;
+            }
+        }
+
+        public int Y
+        {
+            get => y;
+            set
+            {
+                if (value < 0) throw new ArgumentException("y<0!");
+                y = value;
+            }
+        }
+
         public void Draw(Graphics g)
         {
-            g.FillRectangle(Brushes.Red, x, y, w, h);
-            g.DrawRectangle(Pens.Black, x, y, w, h);
+            g.FillRectangle(Brushes.Red, X, Y, w, h);
+            g.DrawRectangle(Pens.Black, X, Y, w, h);
         }
     }
 }
